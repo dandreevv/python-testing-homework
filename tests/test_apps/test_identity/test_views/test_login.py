2b56ad0c343eb607ac7ed634fdab7eadf5af2b83
Daniil Andreev <d2234@mail.ru>
2023-09-16 18:48:34 +0300

    my results

---
+++ tests/test_apps/test_identity/test_views/test_login.py
@@ -0,0 +1,36 @@
+from http import HTTPStatus
+
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+from tests.plugins.identity.users import CreateUserFactory
+
+
+@pytest.mark.django_db()
+def test_login_user_positive(
+    client: Client,
+    create_new_user_factory: 'CreateUserFactory',
+) -> None:
+    user_info = create_new_user_factory()
+    response = client.post(
+        reverse('identity:login'),
+        data={
+            'username': user_info['email'],
+            'password': user_info['password'],
+        },
+    )
+    assert response.status_code == HTTPStatus.FOUND
+
+
+@pytest.mark.django_db()
+def test_login_user_negative(
+    client,
+    create_new_user_factory: 'CreateUserFactory',
+) -> None:
+    user_info = create_new_user_factory()
+    response = client.post(
+        reverse('identity:login'),
+        data={'username': user_info['email'], 'password': 'some_invalid_pass'},
+    )
+    assert response.status_code == HTTPStatus.OK
